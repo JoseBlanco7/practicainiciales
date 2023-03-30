e3796ce98c7a0d81713e1815a6ffdf56131e9c4d
JoseBlanco7 <cjbg@gmail.com>
2023-03-30 03:03:53 -0600

    Commit de perfir

--- BACKEND/LOGIN/app.js
+++ BACKEND/LOGIN/app.js
@@ -135,12 +135,116 @@ app.get('/logout',(req, res)=>{
         res.redirect('/login')
     });
 })
-//14 perfiles
-app.get('/perfiles', (req, res)=>{
-    res.render('perfiles');
+//14 rutaperfiles
+app.get('/perfil', (req, res)=>{
+    res.render('perfil');
+}) 
+
+//15 registro perfiles
+app.post("/perfil", async (req, res)=>{
+    const Nombre = req.body.Nombre;
+    const Edad = req.body.Edad;
+    const CursoAP = req.body.CursoAP;
+    const Carrera = req.body.Carrera;
+    const Estado = req.body.Estado;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass, 8);
+    connection.query("INSERT INTO perfiles SET ?", {Nombre:Nombre, Edad:Edad, CursoAP:CursoAP, Carrera:Carrera, Estado:Estado, pass:passwordHaash}, async (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.render("perfil",{  
+                alert: true,
+                alertTitle: "SE REGISTRO CON EXITO",
+                alertMessage: "¡BUEN TRABAJO!",
+                alertIcon: "success",
+                showConfirmButton:false,
+                timer:1500,
+                ruta: ""
+
+            })
+        }
+    })
+})
+
+//16 rutaverperfil
+app.get('/verperfil', (req, res)=>{
+    res.render('verperfil');
+})
+
+//17 autentificacion de perfiles
+app.post("/autth", async (req, res) =>{
+    const Nombre = req.body.Nombre;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass,  8);
+    if(Nombre && pass){
+       connection.query("SELECT * FROM perfiles WHERE Nombre = ?", [Nombre], async (error, results)=>{
+            if(results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
+                 res.render('verperfil',{
+                    alert: true,
+                    alertTitle: "ERROR",
+                    alertMessage: "USUARIO O CONTRASEÑA INCORRECTOS",
+                    alertIcon: "error",
+                    showConfirmButton: true,
+                    timer: false,
+                    ruta : 'verperfil'
+                 });
+            }else{
+                req.session.loggedin =true;
+                req.session.Nombre = results[0].Nombre
+                req.session.Edad = results[0].Edad
+                req.session.CursoAP = results[0].CursoAP
+                req.session.Carrera = results[0].Carrera
+                req.session.Estado = results[0].Estado
+                res.render("verperfil",{
+                    alert: true,
+                    alertTitle: "CONEXION EXITOSA",
+                    alertMessage: "LOGIN CORRECTO",
+                    alertIcon: "success",
+                    showConfirmButton: false,
+                    timer: 1500,
+                    ruta : "perfilees"
+                 });
+            }
+       })
+    }else{
+        res.render("verperfil",{
+            alert: true,
+            alertTitle: "Advertencia",
+            alertMessage: "AGREGAR USUARIO O CONTRASEÑA",
+            alertIcon: "warning",
+            showConfirmButton: false,
+            timer: 1500,
+            ruta : "verperfil"
+         });
+    }
 }) 
 
 
+//autentificacion de nombre
+app.get('/perfilees',(req, res)=>{
+    if(req.session.loggedin){
+        res.render('perfilees',{
+            verperfil: true,
+            Nombre: req.session.Nombre,
+            Edad: req.session.Edad,
+            CursoAP: req.session.CursoAP,
+            Carrera: req.session.Carrera,
+            Estado: req.session.Estado
+        });
+    } 
+})   
+//autentificacion de edad
+app.get('/perfilees',(req, res)=>{
+    if(req.session.loggedin){
+        res.render('perfilees',{
+            verperfil: true,
+            Edad: req.session.Edad
+        });
+    } 
+})   
+ 
+
 app.listen(3000, (req, res)=>{
     console.log("listening on http://localhost:3000/inicio")
-})  
+})   
--- BACKEND/LOGIN/database/db.js
+++ BACKEND/LOGIN/database/db.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER,
+    perfi: process.env.DB_PERFIL,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 
